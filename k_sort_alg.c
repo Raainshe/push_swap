89b576d25f4e96c700e9a4cf4b5a41ef5526e732
Ryan Makoni <rmakoni@1-E-23.42heilbronn.de>
2024-12-14 17:03:02 +0100

    k sort alg complete

--- k_sort_alg.c
+++ k_sort_alg.c
@@ -6,11 +6,59 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:28:53 by rmakoni           #+#    #+#             */
-/*   Updated: 2024/12/10 17:21:59 by rmakoni          ###   ########.fr       */
+/*   Updated: 2024/12/14 16:44:55 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
+
+//finds the largest and sends position;
+int	shortest_route(t_list *stack_b)
+{
+	int	size;
+	int	i;
+	int	pos_of_largest;
+	int	largest;
+
+	i = 0;
+	size = ft_lstsize(stack_b);
+	largest = (int)stack_b->content;
+	while (stack_b != NULL)
+	{
+		if (stack_b->content > largest)
+		{
+			largest = (int)stack_b->content;
+			pos_of_largest = i;
+		}
+		stack_b = stack_b->next;
+		i++;
+	}
+	if (pos_of_largest <= size / 2)
+		return (pos_of_largest);
+	else
+		return (pos_of_largest - size);
+}
+
+void	send_to_a(t_list **stack_a, t_list **stack_b)
+{
+	int	direction;
+
+	while (ft_lstsize(stack_b) != 0)
+	{
+		direction = shortest_route(stack_b);
+		if (direction > 0)
+		{
+			while (direction--)
+				rb(stack_b);
+		}
+		else
+		{
+			while (direction++)
+				rrb(stack_b);
+		}
+		pa(stack_a, stack_b);
+	}
+}
 
 void	k_sort(t_list *stack_a, t_list *stack_b)
 {
@@ -35,5 +83,5 @@ void	k_sort(t_list *stack_a, t_list *stack_b)
 		else
 			ra(&stack_a);
 	}
-	print_values(stack_b);
+	send_to_a(&stack_a, &stack_b);
 }
